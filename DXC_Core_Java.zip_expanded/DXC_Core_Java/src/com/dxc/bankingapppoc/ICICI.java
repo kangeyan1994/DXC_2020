c119eddf1aa346b459784043ef66a16abf198ecd
kangeyan.vdxc.com <kangeyan.vdxc.com@DESKTOP-QB3B1PV>
2020-06-18 16:29:27 +0100

    commit

---
+++ DXC_Core_Java.zip_expanded/DXC_Core_Java/src/com/dxc/bankingapppoc/ICICI.java
@@ -0,0 +1,31 @@
+package com.dxc.bankingapppoc;
+
+import java.io.BufferedReader;
+
+public class ICICI implements RBI{
+
+	@Override
+	public void openAccount(BufferedReader buff) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deposit(BufferedReader buff) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void withdrawl(BufferedReader buff) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void openFD(BufferedReader buff) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
